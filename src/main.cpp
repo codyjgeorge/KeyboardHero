74041b2ddb075fd4a50d987467495470ff1cb4c8
CodyG <codygeorge315@gmail.com>
2026-01-18 02:46:53 -0600

    game menu cmatrix background drops red letter every 13th letter

--- src/main.cpp
+++ src/main.cpp
@@ -118,7 +118,7 @@ int main() {
       startWindow.draw(titleText);
       startWindow.draw(titleSubtext);
 
-      // create matrix-style background
+      // create matrix-style background - start menu
       if (spawnClock.getElapsedTime().asSeconds() > spawnInterval) {
         Text randomLetterText(firacode);
         char random_letter = random_letter_generator();
@@ -134,7 +134,6 @@ int main() {
         // add letters to vector with random speed
         float randomSpeed = random_speed();
         randomLetters.push_back({randomLetterText, randomSpeed});
-
         spawnClock.restart();
       }
 
@@ -145,7 +144,7 @@ int main() {
         startWindow.draw(letter.randomLetterText);
       }
 
-      // update window
+      // update start window
       startWindow.display();
     }
   } // END start menu loop
@@ -153,18 +152,83 @@ int main() {
   // create game window
   RenderWindow window(VideoMode({1920, 1080}), "Keyboard Hero");
 
+  // create menu title
+  titleText.setPosition({window.getSize().x / 2.f, window.getSize().y / 4.f});
+
+  // create menu text
+  Text menuText(firacode);
+  menuText.setString("ENTER -> Play\n\nR -> Rules\n\nS -> Settings\n\nC -> "
+                     "Controls\n\nQ -> Quit");
+  menuText.setCharacterSize(36);
+  menuText.setFillColor(Color::White);
+  FloatRect menuTextSize = menuText.getLocalBounds();
+  menuText.setOrigin({menuTextSize.size.x / 2.f, menuTextSize.size.y / 2.f});
+  menuText.setPosition({window.getSize().x / 2.f, window.getSize().y / 1.5f});
+
   // start game loop //
   while (window.isOpen()) {
-    while (const auto event = window.pollEvent()) {
-      if (event->is<Event::Closed>()) {
-        window.close();
+
+    // start delta time
+    float dt = gameClock.getElapsedTime().asSeconds();
+    if (dt >= 1.f / 60.f) {
+
+      gameClock.restart();
+
+      // start event handling
+      while (const auto event = window.pollEvent()) {
+        if (event->is<Event::Closed>()) {
+          window.close();
+        }
+
+        // keyboard input - key pressed //
+        if (auto *key = event->getIf<sf::Event::KeyPressed>()) {
+
+          // Q -> quit
+          if (key->scancode == Keyboard::Scancode::Q) {
+            window.close();
+          }
+        } // END key pressed handling
+      } // END game loop event handling
+      window.clear();
+
+      // draw game menu titling text
+      window.draw(titleText);
+      window.draw(menuText);
+
+      // create matrix-style background - game menu
+      if (spawnClock.getElapsedTime().asSeconds() > spawnInterval) {
+        Text randomLetterText(firacode);
+        char random_letter = random_letter_generator();
+        randomLetterText.setString(random_letter);
+        randomLetterText.setCharacterSize(36);
+        randomLetterText.setFillColor(Color::Green);
+        // every 13th letter drops red
+        if (randomLetters.size() % 13 == 0) {
+          randomLetterText.setFillColor(Color::Red);
+        }
+        FloatRect randomLetterTextSize = randomLetterText.getLocalBounds();
+        randomLetterText.setOrigin({randomLetterTextSize.size.x / 2.f,
+                                    randomLetterTextSize.size.y / 2.f});
+        float randomXvalue = random_X_value(window.getSize().x);
+        randomLetterText.setPosition({randomXvalue, -20.f});
+
+        // add letters to vector with random speed
+        float randomSpeed = random_speed();
+        randomLetters.push_back({randomLetterText, randomSpeed});
+
+        spawnClock.restart();
       }
-    }
 
-    window.clear();
+      // update all falling letters
+      float spawnDT = gameClock.restart().asSeconds();
+      for (auto &letter : randomLetters) {
+        letter.randomLetterText.move({0, letter.randomLetterSpeed * dt});
+        window.draw(letter.randomLetterText);
+      }
 
-    window.display();
-  }
+      window.display();
+    } // END delta time
+  } // END game loop
 
   return 0;
 }
