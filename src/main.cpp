a5e1c64118526c3c7bd58a3188342ebcdb6b159b
CodyG <codygeorge315@gmail.com>
2026-01-18 03:25:48 -0600

    deleted redundant start menu

--- src/main.cpp
+++ src/main.cpp
@@ -32,10 +32,9 @@ int main() {
     return -1;
   }
 
-  // create start menu window
-  RenderWindow startWindow(VideoMode({1920, 1080}), "Start Menu",
-                           State::Windowed, settings);
-  startWindow.setVerticalSyncEnabled(true);
+  // create game window
+  RenderWindow window(VideoMode({1920, 1080}), "Keyboard Hero");
+  window.setVerticalSyncEnabled(true);
 
   // create title text
   Text titleText(firacode);
@@ -45,19 +44,17 @@ int main() {
   titleText.setFillColor(Color::White);
   FloatRect titleTextSize = titleText.getLocalBounds();
   titleText.setOrigin({titleTextSize.size.x / 2.f, titleTextSize.size.y / 2.f});
-  titleText.setPosition(
-      {startWindow.getSize().x / 2.f, startWindow.getSize().y / 3.f});
+  titleText.setPosition({window.getSize().x / 2.f, window.getSize().y / 4.f});
 
-  // create title subtext
-  Text titleSubtext(firacode);
-  titleSubtext.setString("Press ENTER");
-  titleSubtext.setCharacterSize(48);
-  titleSubtext.setFillColor(Color::White);
-  FloatRect titleSubtextSize = titleSubtext.getLocalBounds();
-  titleSubtext.setOrigin(
-      {titleSubtextSize.size.x / 2.f, titleSubtextSize.size.y / 2.f});
-  titleSubtext.setPosition(
-      {startWindow.getSize().x / 2.f, startWindow.getSize().y / 1.5f});
+  // create menu text
+  Text menuText(firacode);
+  menuText.setString("ENTER -> Play\n\nR -> Rules\n\nS -> Settings\n\nC -> "
+                     "Controls\n\nQ -> Quit");
+  menuText.setCharacterSize(36);
+  menuText.setFillColor(Color::White);
+  FloatRect menuTextSize = menuText.getLocalBounds();
+  menuText.setOrigin({menuTextSize.size.x / 2.f, menuTextSize.size.y / 2.f});
+  menuText.setPosition({window.getSize().x / 2.f, window.getSize().y / 1.5f});
 
   // sound buffers
   SoundBuffer titleSongBuffer;
@@ -86,85 +83,6 @@ int main() {
   titleSong.play();
   bool isLooping = titleSong.isLooping();
 
-  // start start menu loop
-  while (startWindow.isOpen()) {
-
-    // start delta time
-    float dt = gameClock.getElapsedTime().asSeconds();
-    if (dt >= 1.f / 60.f) {
-
-      gameClock.restart();
-
-      // start event handling
-      while (const auto event = startWindow.pollEvent()) {
-        if (event->is<Event::Closed>()) {
-          startWindow.close();
-        }
-
-        // keyboard input - key pressed //
-        if (auto *key = event->getIf<sf::Event::KeyPressed>()) {
-
-          // ENTER -> close start screen, begin game
-          if (key->scancode == Keyboard::Scancode::Enter) {
-            startWindow.close();
-            titleSong.stop();
-            titleTransSound.play();
-          }
-        } // END key pressed handling
-      } // END start menu event handling
-      startWindow.clear();
-
-      // draw start menu titling text
-      startWindow.draw(titleText);
-      startWindow.draw(titleSubtext);
-
-      // create matrix-style background - start menu
-      if (spawnClock.getElapsedTime().asSeconds() > spawnInterval) {
-        Text randomLetterText(firacode);
-        char random_letter = random_letter_generator();
-        randomLetterText.setString(random_letter);
-        randomLetterText.setCharacterSize(36);
-        randomLetterText.setFillColor(Color::Green);
-        FloatRect randomLetterTextSize = randomLetterText.getLocalBounds();
-        randomLetterText.setOrigin({randomLetterTextSize.size.x / 2.f,
-                                    randomLetterTextSize.size.y / 2.f});
-        float randomXvalue = random_X_value(startWindow.getSize().x);
-        randomLetterText.setPosition({randomXvalue, -20.f});
-
-        // add letters to vector with random speed
-        float randomSpeed = random_speed();
-        randomLetters.push_back({randomLetterText, randomSpeed});
-        spawnClock.restart();
-      }
-
-      // update all falling letters
-      float spawnDT = gameClock.restart().asSeconds();
-      for (auto &letter : randomLetters) {
-        letter.randomLetterText.move({0, letter.randomLetterSpeed * dt});
-        startWindow.draw(letter.randomLetterText);
-      }
-
-      // update start window
-      startWindow.display();
-    }
-  } // END start menu loop
-
-  // create game window
-  RenderWindow window(VideoMode({1920, 1080}), "Keyboard Hero");
-
-  // create menu title
-  titleText.setPosition({window.getSize().x / 2.f, window.getSize().y / 4.f});
-
-  // create menu text
-  Text menuText(firacode);
-  menuText.setString("ENTER -> Play\n\nR -> Rules\n\nS -> Settings\n\nC -> "
-                     "Controls\n\nQ -> Quit");
-  menuText.setCharacterSize(36);
-  menuText.setFillColor(Color::White);
-  FloatRect menuTextSize = menuText.getLocalBounds();
-  menuText.setOrigin({menuTextSize.size.x / 2.f, menuTextSize.size.y / 2.f});
-  menuText.setPosition({window.getSize().x / 2.f, window.getSize().y / 1.5f});
-
   // start game loop //
   while (window.isOpen()) {
 
@@ -178,6 +96,7 @@ int main() {
       while (const auto event = window.pollEvent()) {
         if (event->is<Event::Closed>()) {
           window.close();
+          titleSong.stop();
         }
 
         // keyboard input - key pressed //
@@ -186,9 +105,18 @@ int main() {
           // Q -> quit
           if (key->scancode == Keyboard::Scancode::Q) {
             window.close();
+            titleSong.stop();
+          }
+          // ENTER -> close start screen, begin game
+          if (key->scancode == Keyboard::Scancode::Enter) {
+            window.close();
+            titleSong.stop();
+            titleTransSound.play();
           }
         } // END key pressed handling
       } // END game loop event handling
+
+      // start drawing - menu window
       window.clear();
 
       // draw game menu titling text
